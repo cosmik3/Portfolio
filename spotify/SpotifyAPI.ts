8356d161360e2d9cd36811dd6842f061ba21f4b3
Manu R <manurajbhar12@gmail.com>
2024-05-15 10:45:53 +0530

    Revert "Refactored SpotifyAPI.ts file and removed unnecessary files ✅ (#7)"
    This reverts commit ea10e57de028f62a36deab30139184d4a18a4a9b.

--- spotify/SpotifyAPI.ts
+++ spotify/SpotifyAPI.ts
@@ -5,11 +5,8 @@ const client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 const client_secret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.NEXT_PUBLIC_SPOTIFY_REFRESH_TOKEN
 
-const ENDPOINTS = {
-    NOW_PLAYING_ENDPOINT : 'https://api.spotify.com/v1/me/player/currently-playing',
-    TOKEN_ENDPOINT : 'https://accounts.spotify.com/api/token',
-    RECENTLY_PLAYED_ENDPOINT: 'https://api.spotify.com/v1/me/player/recently-played'
-}
+const NOW_PLAYING_ENDPOINT = 'https://api.spotify.com/v1/me/player/currently-playing';
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 
@@ -47,7 +44,7 @@ export interface LastPlayedSong
  
 export const getAccessToken = async () => 
   {
-    const response = await axios.post(ENDPOINTS.TOKEN_ENDPOINT, querystring.stringify({
+    const response = await axios.post(TOKEN_ENDPOINT, querystring.stringify({
       grant_type: 'refresh_token',
       refresh_token,
     }), {
@@ -65,7 +62,7 @@ export const getNowPlaying = async ( access_token: string ) : Promise < CurrentM
 {
     console.log(access_token)
     try {
-      const response = await axios.get<CurrentMusic>(ENDPOINTS.NOW_PLAYING_ENDPOINT, {
+      const response = await axios.get<CurrentMusic>(NOW_PLAYING_ENDPOINT, {
         headers: {
           Authorization: `Bearer ${access_token}`,
         },
@@ -89,7 +86,7 @@ export const getNowPlaying = async ( access_token: string ) : Promise < CurrentM
  export const getRecentlyPlayed = async (accessToken: string) : Promise < LastPlayedSong | null > => 
  {
     try {
-      const response = await axios.get(ENDPOINTS.RECENTLY_PLAYED_ENDPOINT, {
+      const response = await axios.get('https://api.spotify.com/v1/me/player/recently-played', {
         headers: {
           'Authorization': `Bearer ${accessToken}`
         }
